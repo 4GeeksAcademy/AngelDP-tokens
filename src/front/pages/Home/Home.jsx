9d0641a6ea73287320ddfa97f09599a5c88e294c
Miguel Toyas Pernichi <mitoperni@mac.home>
2025-03-26 16:47:30 +0000

    creando fallos

--- src/front/pages/Home/Home.jsx
+++ src/front/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import useGlobalReducer from "../../hooks/useGlobalReducer";
 import "./Home.css";
 
-export const API_STARWARS = "https://sturdy-goggles-jj4495pppj55h5667-3001.app.github.dev/api";
+export const API_STARWARS_1 = "https://sturdy-goggles-jj4495pppj55h5667-3001.app.github.dev/api";
 
 export const Home = () => {
     const { store, dispatch } = useGlobalReducer();
@@ -11,7 +11,7 @@ export const Home = () => {
     useEffect(() => {
         const getCategories = async () => {
             try {
-                const response = await fetch(API_STARWARS + "/clases");
+                const response = await fetch(API_STARWARS_1 + "/clases");
                 if (!response.ok) {
                     throw new Error("We can't get the categories");
                 }
